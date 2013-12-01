96ac5b08b89ecba7e7b6eee09b939f320871c016
Charlie Anderson <charlie.d.anderson@gmail.com>
2013-12-01 19:37:01 +0000

    update test.sh, to run many tests. This bastard is slow!

--- program/test.sh
+++ program/test.sh
@@ -2,13 +2,25 @@
 
 javac *.java
 
+echo
+echo "Sample Set"
+
 java TrainClassifier ../automark/sampletrain
 java filter ../automark/sampletest/1.txt
 java filter ../automark/sampletest/2.txt
 
+echo
+echo "3cool Set"
 java TrainClassifier ../automark/train3cool
 java filter ../automark/test3cool/1.txt
 java filter ../automark/test3cool/2.txt
 
+echo
+echo "Part 2 Set"
 java TrainClassifier ../train
 time java filter ../automark2/sampletest/1.txt
+for i in {1..10}; do
+  time java filter "../train/ham$i.txt"
+  time java filter "../train/spam$i.txt"
+done
+
