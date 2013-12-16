b6d3226b1439a7fb62e452d9e1be91a6e796e0f9
Adam Renak <ar1406@my.bristol.ac.uk>
2013-12-16 12:22:10 +0000

    Prints out totals for confusion matrices

--- program/crossValidate.sh
+++ program/crossValidate.sh
@@ -25,22 +25,33 @@ do
   done
   java TrainClassifier $dir
   # test on this set
+  HAMCORRECT=0
+  TOTALHAM=0
+  SPAMCORRECT=0
+  TOTALSPAM=0
   CORRECT=0
   TOTAL=0
   for f in ../train/$i/spam*.txt
   do
     TOTAL=$((TOTAL+1))
+    TOTALSPAM=$((TOTALSPAM+1))
     if [ "$(java filter $f)" = "spam" ] ; then
+      SPAMCORRECT=$((SPAMCORRECT+1))
       CORRECT=$((CORRECT+1))
     fi
   done
   for f in ../train/$i/ham*.txt
   do
     TOTAL=$((TOTAL+1))
+    TOTALHAM=$((TOTALHAM+1))
     if [ "$(java filter $f)" = "ham" ] ; then
+      HAMCORRECT=$((HAMCORRECT+1))
       CORRECT=$((CORRECT+1))
     fi
   done
   # output number of correctly classified instances / total number of instances
   echo "Fold $i          $CORRECT out of $TOTAL"
+  echo "CORRECTLY IDENTIFIED SPAM: $SPAMCORRECT out of $TOTALSPAM"
+  echo "CORRECTLY IDENTIFIED HAM:  $HAMCORRECT out of $TOTALHAM"
+
 done
